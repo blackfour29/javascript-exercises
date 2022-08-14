2a651805cd9b9f4477be34669673661308ba73cd
gabi <gaby.gabyyy29@gmail.com>
2022-08-14 22:11:46 +0300

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArray, ...numbers) {
+let newArray = [];
+for(element of originalArray )
+{
+    // console.log(!originalArray.includes(1));
+    if(numbers.includes(element))
+        continue;
+    else
+        newArray.push(element);
+}
+return newArray;
 };
+
+//console.log(removeFromArray([3,2,1]));
 
 // Do not edit below this line
 module.exports = removeFromArray;

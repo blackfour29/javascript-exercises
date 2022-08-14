86c0d97d4eb69c25411b2d8ebcb96fd4ee7fe7c1
gabi <gaby.gabyyy29@gmail.com>
2022-08-14 21:08:42 +0300

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    let result="";
+    if (times < 0)
+        return "ERROR";
+    else if(times === 0)
+        return "";
+    else
+    { 
+        for(let i=1; i<=times; i++)
+        {
+            result+=string;
+        }
+        return result;
+    }       
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;

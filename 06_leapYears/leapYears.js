3ac7f6689dbb00b19c72320ce6ab2e4ca0282623
gabi <gaby.gabyyy29@gmail.com>
2022-08-14 23:25:27 +0300

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if(typeof(year) !== "number")
+        return "ERROR";
+
+    if(year % 400 === 0)
+        return true;
+    else if( year % 4 === 0 && year % 100 !== 0)
+        return true;
+    else
+        return false;
 };
+
+console.log(leapYears(2004));
 
 // Do not edit below this line
 module.exports = leapYears;

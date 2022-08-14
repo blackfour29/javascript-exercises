d7f4d4d4238b19a991a7fce5a245916229c03030
gabi <gaby.gabyyy29@gmail.com>
2022-08-14 23:47:40 +0300

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,24 @@
-const ftoc = function() {
-
+const ftoc = function(tempf) { 
+  let result = ((tempf-32) * 0.5556);
+  if(result % 1 === 0)
+    return result;
+  else
+  {
+    console.log(result);
+    return parseFloat(result.toFixed(1));
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(tempc) {
+  result = ((tempc*1.8) + 32);
+  if(result % 1 === 0)
+    return result;
+  else
+    return parseFloat(result.toFixed(1));
 };
+
+// console.log(ctof(30));
+// console.log(ftoc(100))
 
 // Do not edit below this line
 module.exports = {

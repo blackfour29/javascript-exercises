8605df9e2e1e2692034dbe29ea2d518fce21b62d
gabi <gaby.gabyyy29@gmail.com>
2022-08-14 23:14:08 +0300

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(lowerLimit, upperLimit) {
+    console.log(typeof(lowerLimit), typeof(upperLimit))
+    if(typeof(lowerLimit) !== "number" || typeof(upperLimit)!== "number" || lowerLimit < 0 || upperLimit < 0)
+    {
+        return "ERROR";
+    }
+    if(upperLimit < lowerLimit)
+    {
+        let temp = upperLimit;
+        upperLimit = lowerLimit;
+        lowerLimit = temp;
+    }
 
+    let sum = 0;
+
+    for(let i=lowerLimit; i<=upperLimit; i++)
+        sum+=i;
+
+    return sum;
 };
+
+console.log(sumAll(1,"4"));
 
 // Do not edit below this line
 module.exports = sumAll;

8ef4764ef7cc6b28afc6d6d488c0bab13da408dd
gabi <gaby.gabyyy29@gmail.com>
2022-08-16 02:45:54 +0300

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number < 0)
+        return "OOPS";
+    
+    if(typeof(number) === "string")
+        number = parseInt(number);
 
+    let a=1, b=1;
+
+    if(number === 1)
+        return 1;
+    else if(number === 2)
+        return 1;
+    else
+    {
+        while(number>2)
+        {
+            let temp = b;
+            b= a+b;
+            a=temp;
+            number--;
+        }
+    }
+
+    return b;
 };
+
+console.log(fibonacci(5));
 
 // Do not edit below this line
 module.exports = fibonacci;

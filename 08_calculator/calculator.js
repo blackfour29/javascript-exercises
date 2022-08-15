707de029344a936c9b8518c348f786e7585ad9ba
gabi <gaby.gabyyy29@gmail.com>
2022-08-16 01:47:40 +0300

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,62 @@
-const add = function() {
-	
+const add = function(...arr) {
+  let s=0;
+  for(number of arr)
+    s+=number;
+  return s;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let s=0;  
+
+  s = arr.reduce(function(total, currentNumber){
+    return total+=currentNumber;
+  }, 0)
+
+  return s;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let p = 1;
 
+  for(number of arr)
+  {
+    p*=number;
+  }
+
+  return p;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  let result = base;
+	while(exp>1)
+  {
+    result *= base;
+    exp--;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n === 1 || n === 0)
+    return 1;
+  else
+  {
+    let p=1;
+    
+    while(n)
+    {
+      p*=n;
+      n--;
+    } 
+    return p;
+  }
 };
+
+// console.log(sum(2,3));
 
 // Do not edit below this line
 module.exports = {

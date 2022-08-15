dcad46fca7f05d34a2f13ef3d24c2d56ccfd1ce9
gabi <gaby.gabyyy29@gmail.com>
2022-08-16 02:24:03 +0300

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringArr = string
+                    .toLowerCase()
+                    .replace(/[.,/#!$%^&*;:{}=-_`~()]/g, "")
+                    .replace(/s{2,}/g, " ")
+                    .split(" ")
+                    .join("");
 
+    let reversedArr = Array.from(stringArr).reverse().toString().split(",").join("");
+
+    
+    return stringArr.toString() === reversedArr.toString();
 };
+
+console.log(palindromes("Animal loots foliated detail of stool lamina."));
 
 // Do not edit below this line
 module.exports = palindromes;

fd4e64463945f0bf75dfc5e9a669c393b15e7d39
gabi <gaby.gabyyy29@gmail.com>
2022-08-16 03:14:08 +0300

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,37 @@
-const getTheTitles = function() {
+const getTheTitles = function(objArr) {
+    
+    // option 1
 
+    // let titlesArr = [];
+
+    // objArr.forEach( (obj) => {
+    //     titlesArr.push(obj.title);
+    // })
+
+    // return titlesArr;
+
+
+    // option 2
+    let titlesArr = [];
+
+    titlesArr = objArr.map(function(currentElement){
+        return currentElement.title;
+    });
+
+    return titlesArr
 };
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+//   console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
